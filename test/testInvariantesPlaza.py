f36ca23e34e6456f4372829a3e1aa42812f84b94
Belen Tracchia <tracchiabelen@gmail.com>
2022-05-18 17:08:49 -0300

    Test de invariantes de plaza modificados

--- test/testInvariantesPlaza.py
+++ test/testInvariantesPlaza.py
@@ -1,24 +1,21 @@
 for i in open("./test/InvariantesPlaza"):
     vectorMarcado = [int(j) for j in  i.split(" ")[0:-1] ]
     
-    if vectorMarcado[1]+vectorMarcado[7] != 1:
-        print("ERROR: plaza[1] + plaza[7] != 1")
+    if vectorMarcado[0]+vectorMarcado[4] != 3:
+        print("ERROR: plaza[0] + plaza[5] != 3")
         exit()
 
-    if vectorMarcado[4] + vectorMarcado[12] + vectorMarcado[14] != 1:
+    if vectorMarcado[1] + vectorMarcado[3] != 2:
         print("ERROR: plaza[4] + plaza[12] + plaza[14] != 1")
         exit()
 
-    if vectorMarcado[5] + vectorMarcado[13] + vectorMarcado[15] != 1:
+    if vectorMarcado[3] + vectorMarcado[4] + vectorMarcado[5] != 1:
         print("ERROR: plaza[5] + plaza[13] + plaza[15] != 1")
         exit()
 
-    if vectorMarcado[0] + vectorMarcado[6] != 1:
+    if vectorMarcado[2] + vectorMarcado[3] + vectorMarcado[4]  + vectorMarcado[6] != 5:
         print("ERROR: plaza[0] + plaza[6]!= 1")
         exit()
-    
-    if(vectorMarcado[8]+vectorMarcado[9] != 1):
-        print("ERROR: plaza[8] + plaza[9] != 1")
-        exit()
+
 
 print("Los invariantes de plaza son correctos")
